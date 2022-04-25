8940f19c2d5c24ae37e7a01b36a01f5303c0fecc
Xyzin503 <xyzin503@gmail.com>
2022-04-25 08:37:04 +0000

    Added general body layout and color. Added text with glow effect.

---
+++ simpleTodo/imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const tasks = [
+  {_id:1,text: 'fucking tits'},
+  {_id:2, text: 'yes they are'},
+  {_id:3, text: 'we are getting there'},
+  {_id:4, text: 'keeep buiing'},
+
+];
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    
+   
+  </div>
+);
---
+++ simpleTodo/imports/ui/List.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const List = ({task}) => {
+    return <div className="container">
+        <ul>
+        {this.props.task};
+        </ul>
+        <h1>DONE</h1>
+    </div>
+};
